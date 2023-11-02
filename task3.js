0c8e803950c940098371dfb679db9f55b1cc8266
Anna Oliinyk <annaoliinyk@Annas-MacBook-Pro.local>
2023-11-02 23:24:24 +0200

    complete task 3

--- task3.js
+++ task3.js
@@ -1,6 +1,20 @@
-/**
-Напишіть функцію changeCSS, яка спрацьовуватиме по кліку на кнопку
-і змінюватиме стиль вмісту тега <p>:
-колір шрифту – оранжевий, розмір шрифту 20px, шрифт сімейства "Comic Sans MS"
- */
-function changeCSS() {}
+
+let buttons = document.querySelectorAll("button");
+buttons[0].addEventListener("click", function(event){
+    document.body.style.backgroundColor = "blue";
+}) 
+buttons[1].addEventListener("dblclick", function(event){
+    document.body.style.backgroundColor = "pink";
+}) 
+buttons[2].addEventListener("mousedown", function(event){
+    document.body.style.backgroundColor = "#342415";
+}) 
+buttons[2].addEventListener("mouseup", function(event){
+    document.body.style.backgroundColor = "white";
+}) 
+document.querySelector("a").addEventListener("mouseenter", function(event){
+    document.body.style.backgroundColor = "yellow";
+})
+document.querySelector("a").addEventListener("mouseleave", function(event){
+    document.body.style.backgroundColor = "white";
+})
