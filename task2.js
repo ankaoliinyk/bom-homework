d1d623704f0bda60c1287fbdb2c9faa88c1b423b
Anna Oliinyk <annaoliinyk@Annas-MacBook-Pro.local>
2023-11-02 23:02:33 +0200

    complete task 2

--- task2.js
+++ task2.js
@@ -3,4 +3,12 @@
 і змінюватиме стиль вмісту тега <p>:
 колір шрифту – оранжевий, розмір шрифту 20px, шрифт сімейства "Comic Sans MS"
  */
-function changeCSS() {}
+function changeCSS() {
+    let el = document.querySelector("p#text");
+    el.style.color = "orange";
+    el.style.fontSize = "20px";
+    el.style.fontFamily = "Comic Sans MS"
+}
+document.querySelector("button").addEventListener("click", function(event) {
+    changeCSS()
+})
