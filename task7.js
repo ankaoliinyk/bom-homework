8cfe66e8c22e477008f566d2ebed5ec3b4e8bb8f
Anna Oliinyk <annaoliinyk@Annas-MacBook-Pro.local>
2023-11-03 22:12:38 +0200

    complete task 7

--- task7.js
+++ task7.js
@@ -7,3 +7,35 @@
 Також потрібно нижче в параграфі вивести назву обраної країни і місто.
 Спробуйте не вносити зміни в html файл.
  */
+
+let cities = {
+    ukr: ["Kharkiv", "Kyiv", "Lviv"],
+    ger: ["Cologne", "Berlin", "Munich"],
+    usa: ["Los Angeles", "New York", "San Diego"],
+};
+
+let selectCountry = document.querySelector("select#country");
+let selectCity = document.querySelector("select#cities");
+
+selectCountry.addEventListener("change", function(event) {
+    let countryCities = cities[event.target.value];
+    selectCity.innerHTML = "";
+    countryCities.forEach(function(city) {
+        let option = document.createElement("option");
+        option.value = city;
+        option.innerHTML = city;
+        selectCity.appendChild(option)
+    })
+    let e = new Event('change');
+    selectCity.dispatchEvent(e);
+})
+
+selectCity.addEventListener("change", function(event) {
+    let out = document.querySelector("p");
+    let country = document.querySelector("select#country option[value='" + selectCountry.value + "']")
+    out.innerHTML = country.innerHTML + ", " + event.target.value;
+})
+
+let e = new Event('change');
+selectCountry.dispatchEvent(e);
+selectCity.dispatchEvent(e);
