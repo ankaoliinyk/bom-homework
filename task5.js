03d35e8020e07f24e371193fcadc640b3d6aa969
Anna Oliinyk <annaoliinyk@Annas-MacBook-Pro.local>
2023-11-03 20:15:59 +0200

    complete task 5

--- task5.js
+++ task5.js
@@ -6,3 +6,22 @@
 додаткове випробування: обмежити кількість виведених повідомлень до 5,
 при додаванні повіомлення після 5го, зі сторінки має зникнути найстаріше повідомлення.
 */
+let out = document.createElement("div");
+let button = document.querySelector("button");
+document.body.appendChild(out);
+button.addEventListener("click", function(event) {
+    let box = document.createElement("p");
+    box.innerHTML = "I was pressed!";
+    document.querySelector("div").appendChild(box);
+})
+
+button.addEventListener("mouseenter", function(event) {
+    let box = document.createElement("p");
+    box.innerHTML = "Mouse on me!";
+    document.querySelector("div").appendChild(box);
+})
+button.addEventListener("mouseleave", function(event) {
+    let box = document.createElement("p");
+    box.innerHTML = "Mouse is not on me!";
+    document.querySelector("div").appendChild(box);
+})
