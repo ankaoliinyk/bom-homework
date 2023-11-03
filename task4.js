29f41c575e1f013252c04962badc0bd0fe562b1a
Anna Oliinyk <annaoliinyk@Annas-MacBook-Pro.local>
2023-11-03 19:44:48 +0200

    complete task 4

--- task4.js
+++ task4.js
@@ -2,3 +2,11 @@
  Реалізуйте логіку видалення вибраного елементу списку при натисканні на кнопку.
  Видаляти елементи можна в будь якому порядку.
  */
+
+document.querySelector("button").addEventListener("click", function(event) {
+    let select = document.querySelector("select");
+    let selectedOption = select.querySelector("option[value='"+ select.value +"']");
+    if(selectedOption) {
+        selectedOption.remove();
+    }
+})
