af64647fa79c39ff71efb791d1de92ab29b4b207
Anna Oliinyk <annaoliinyk@Annas-MacBook-Pro.local>
2023-11-03 19:12:36 +0200

    complete task 1

--- task1.js
+++ task1.js
@@ -4,3 +4,21 @@
 3. із затримкою 2 сек перемістіть вікно в точку з координатами (200, 200);
 4. із затримкою 2 сек закрийте вікно.
  */
+
+let newTab = window.open(
+    "about:blank",
+    "task1",
+    "width=300,height=300"
+)
+
+setTimeout(function() { 
+  newTab.resizeTo(500, 500);
+}, 2000);
+
+setTimeout(function() { 
+    newTab.moveTo(200, 200);
+}, 4000);
+
+setTimeout(function() { 
+    newTab.close();
+}, 6000);
